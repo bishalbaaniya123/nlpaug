8b467438325de78097b83de5f109c3713b5b8ac4
Bishal Baaniya <bishalbaaniya@gmail.com>
2022-09-07 22:40:49 -0400

    added test cases

--- mtaug/test_augmenter/back_translation.py
+++
@@ -1,6 +0,0 @@
-from nlpaug.augmenter.word import BackTranslationAug
-
-back_translation = BackTranslationAug()
-
-test_text = 'the quick brown fox jumps over a lazy dog'
-print(back_translation.substitute(test_text))
